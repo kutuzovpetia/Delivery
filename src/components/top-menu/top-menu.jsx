82a687fedbca6685aad329579572c3dc5e6d11d1
kutuzovpetia <petiakutuzov@gmail.com>
2021-01-05 12:27:45 +0200

    Якщо корзина не пуста, замена иконки

--- src/components/top-menu/top-menu.jsx
+++ src/components/top-menu/top-menu.jsx
@@ -1,68 +1,98 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import s from "./style.module.scss";
-import heart from '../../image/heart.png';
-import basket from '../../image/basket.png';
+import heart from "../../image/heart.png";
+import basket_ from "../../image/basket_.png";
+import basket from "../../image/basket.png";
+import {connect} from 'react-redux';
 
+class TopMenu extends Component {
+  constructor(props) {
+    super(props);
 
-export default class TopMenu extends Component {
+    this.state = {
+      openNav: false,
+    };
+  }
 
+  openNav = () => {
+    const { openNav } = this.state;
+    this.setState({
+      openNav: !openNav,
+    });
 
-constructor(props){
-      super(props);
-
-      this.state = {
-        openNav : false,
-      }
-}
-
-openNav = () => {
-  const {openNav} = this.state;
-  this.setState({
-    openNav : !openNav
-  });
-
-  const Order = document.getElementById("mySideOrder");
-  openNav ? Order.style.width = "0" : Order.style.width = "380px";
-}
+    const Order = document.getElementById("mySideOrder");
+    openNav ? (Order.style.width = "0") : (Order.style.width = "380px");
+  };
 
   render() {
-
     return (
+      <nav className={"navbar navbar-expand-lg navbar-dark fixed-top " + s.menu}>
+        <div className={s.containerMy}>
+          {/* <NavLink className="navbar-brand" to="/Главная">Navbar</NavLink> */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-    <nav className={"navbar navbar-expand-lg navbar-dark fixed-top " + s.menu}>
-    <div className={s.containerMy}>
-    {/* <NavLink className="navbar-brand" to="/Главная">Navbar</NavLink> */}
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    
-    <img className='navbar-toggler' src={basket} alt="..." onClick={this.openNav}/>
+          <img
+            className={`navbar-toggler`}
+            src={this.props.order.length === 0 ? basket : basket_}
+            alt="..."
+            onClick={this.openNav}
+          />
 
-    <div className="collapse navbar-collapse justify-content-between" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <NavLink to="home" className={'nav-link'}>Главная</NavLink>
-        <NavLink to="contact" className={'nav-link'}>Контакты</NavLink>
-        <NavLink to="about" className={'nav-link'}>О нас</NavLink>
-      </div>
-      
-      <div className="d-flex navbar-nav align-items-center">
-           <NavLink to="/enter" className={'nav-link activ' + s.menuItem}>Вход</NavLink>
-           <NavLink to="/register" className={'nav-link activ' + s.menuItem}>Регистрация</NavLink>
-           <div className="d-flex">
-             <div className={s.topI}>
-                <img src={heart} alt="..." />
-             </div>
-             <div className={`${s.topI}`}>
-                <img src={basket} alt="..." onClick={this.openNav}/>
-             </div>
+          <div
+            className="collapse navbar-collapse justify-content-between"
+            id="navbarNavAltMarkup"
+          >
+            <div className="navbar-nav">
+              <NavLink to="home" className={"nav-link"}>
+                Главная
+              </NavLink>
+              <NavLink to="contact" className={"nav-link"}>
+                Контакты
+              </NavLink>
+              <NavLink to="about" className={"nav-link"}>
+                О нас
+              </NavLink>
+            </div>
+
+            <div className="d-flex navbar-nav align-items-center">
+              <NavLink to="/enter" className={"nav-link activ" + s.menuItem}>
+                Вход
+              </NavLink>
+              <NavLink to="/register" className={"nav-link activ" + s.menuItem}>
+                Регистрация
+              </NavLink>
+              <div className="d-flex">
+                <div className={s.topI}>
+                  <img src={heart} alt="..." />
+                </div>
+                <div className={s.topI}>
+                  <img src={this.props.order.length === 0 ? basket : basket_} alt="..." onClick={this.openNav} />
+                </div>
+              </div>
+            </div>
           </div>
-      </div>
-    </div>
-  </div>
-</nav>
-
-      /*****************************************************/
+        </div>
+      </nav>
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  // console.log(state.order.length);
+  return{
+      order: state.order
+  }
+}
+
+export default connect(mapStateToProps)(TopMenu);
